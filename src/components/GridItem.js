fbc1b749cf054a78ad7ce07a67b1eea2e23bf164
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-09-26 22:15:25 -0700

    added GridItem component + styles

---
+++ src/components/GridItem.js
@@ -0,0 +1,28 @@
+import { Container, Icon, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    gridItem: {
+        padding: 50
+    }
+}))
+
+const GridItem = ({ title, description, icon }) => {
+
+    const { gridItem } = useStyles();
+
+    return ( 
+        <Container align="left" className={gridItem}>
+            <Icon>
+                {icon}
+            </Icon>
+            <Typography variant="h5">
+                {title}
+            </Typography>
+            <Typography>
+                {description}
+            </Typography>
+        </Container>
+    );
+}
+
+export default GridItem;
--- src/components/ProductInformation.js
+++ src/components/ProductInformation.js
@@ -1,14 +1,33 @@
-import { Container, Grid, Typography } from "@material-ui/core";
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { ThumbUp } from "@material-ui/icons";
+import GridItem from "./GridItem";
+
+const useStyles = makeStyles((theme) => ({
+    mainContainer: {
+        paddingLeft: 40,
+        paddingRight: 40,
+        paddingTop: 120,
+        paddingBottom: 120
+    }
+}));
 
 const ProductInformation = () => {
+
+    const { mainContainer } = useStyles();
+
     return ( 
-        <Container>
-            <Typography>
+        <Container className={mainContainer}>
+            <Typography variant="h2">
                 Let me show you a million dead bodies!
             </Typography>
 
             <Grid container>
                 <Grid item>
+                    <GridItem 
+                        title="so many DEAD bodies!"
+                        description="in all sizes, weights and ethnic groups"
+                        icon={<ThumbUp />}
+                    />
                 </Grid>
             </Grid>
 
