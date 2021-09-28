e2ef0d42e87578b0b97da695ee7e8b1b29374d9a
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-09-27 20:25:34 -0700

    created PricingOptions component + added to Pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,4 +1,5 @@
 import { Container, Grid, Typography } from "@material-ui/core";
+import PricingOption from "./PricingOption";
 
 const Pricing = () => {
     return ( 
@@ -8,14 +9,29 @@ const Pricing = () => {
             </Typography>
 
             <Grid container>
-                <Grid item>
-
+                <Grid lg={4} md={12} sm={12} xs={12} item>
+                    <PricingOption
+                        title="One Dead Person"
+                        price="250"
+                        length="Single"
+                        description="One dead person to your liking"
+                    />
                 </Grid>
-                <Grid item>
-
+                <Grid lg={4} md={12} sm={12} xs={12} item>
+                <PricingOption
+                        title="Two Dead People"
+                        price="450"
+                        length="Couple"
+                        description="Mix and match or make them fight"
+                    />
                 </Grid>
-                <Grid item>
-
+                <Grid lg={4} md={12} sm={12} xs={12} item>
+                <PricingOption
+                        title="A family of Dead"
+                        price="1000"
+                        length="Includes 5 bodies"
+                        description="A whole team of dead people"
+                    />
                 </Grid>
             </Grid>
         </Container>
